9261394064c5d651d1fd24dccb1af2cfa8d7486f
lpoliveiraneto <leonidespires@gmail.com>
2020-11-24 09:52:26 -0300

    tornando a categoria dinâmica.

--- src/components/FormCadastro/FormularioCadastro.jsx
+++ src/components/FormCadastro/FormularioCadastro.jsx
@@ -6,7 +6,13 @@ export class FormularioCadastro extends Component {
     constructor(props){
         super(props);
         this.titulo ="";
-        this.texto = ""
+        this.texto = "";
+        this.categoria = "Sem categoria";
+    }
+
+    _handleMudancaCategoria(evento){
+        evento.stopPropagation();
+        this.categoria = evento.target.value;
     }
 
     _handleMudancaTitulo(evento){
@@ -22,7 +28,7 @@ export class FormularioCadastro extends Component {
     _criarNota(evento){
         evento.preventDefault();
         evento.stopPropagation();
-        this.props.criarNota(this.titulo, this.texto);
+        this.props.criarNota(this.titulo, this.texto, this.categoria);
     }
 
     render() {
@@ -30,7 +36,11 @@ export class FormularioCadastro extends Component {
             <form className="form-cadastro"
                 onSubmit={this._criarNota.bind(this)}>
 
-                <select className="form-cadastro_input">
+                <select onChange={this._handleMudancaCategoria.bind(this)} 
+                    className="form-cadastro_input"
+                >
+                    <option defaultChecked={true}>Sem categoria</option>
+
                     {this.props.categorias.map( categoria => {
                         return <option>{categoria}</option>
                     })}
